72b8fb53f48645ada51f7c5daac65fa29ca4a2a8
BogdanFV <fbogdan13@gmail.com>
2021-12-18 03:47:11 +0300

    git commit 2

--- dist/app.js
+++ dist/app.js
@@ -1072,6 +1072,84 @@
 
   function initJs() {
     // Тут начинается твой js-код
+    let audio,
+      playbtn,
+      mutebtn,
+      seekslider,
+      volumeslider,
+      seeking = false,
+      seekto,
+      curtimetext,
+      durtimetext;
+
+    function initAudioPlayer() {
+      audio = new Audio(
+        'https://cdn.the-village.ru/the-village.ru/assets/sensai-1403/audio_1.mp3',
+      );
+      playbtn = document.getElementById('playpausebtn');
+      mutebtn = document.getElementById('mutebtn');
+      seekslider = document.getElementById('seekslider');
+      // Add Event Handling
+      playbtn.addEventListener('click', playPause);
+      mutebtn.addEventListener('click', mute);
+      seekslider.addEventListener('mousedown', function(event) {
+        seeking = true;
+        seek(event);
+      });
+      seekslider.addEventListener('mousemove', function(event) {
+        seek(event);
+      });
+      seekslider.addEventListener('mouseup', function() {
+        seeking = false;
+      });
+      audio.addEventListener('timeupdate', function() {
+        seektimeupdate();
+      });
+      // Functions
+      function playPause() {
+        if (audio.paused) {
+          audio.play();
+          playbtn.style.background =
+            'url(https://cdn.the-village.ru/the-village.ru/2021/12/17/pausebtn_0.svg) center no-repeat';
+          playbtn.style.backgroundSize = '100% 100%';
+        } else {
+          audio.pause();
+          playbtn.style.background =
+            'url(https://cdn.the-village.ru/the-village.ru/2021/12/17/playbtn_1.svg) center no-repeat';
+          playbtn.style.backgroundSize = '100% 100%';
+        }
+      }
+      function mute() {
+        if (audio.muted) {
+          audio.muted = false;
+          console.log('Вкл аудио');
+          mutebtn.style.background =
+            'url(https://cdn.the-village.ru/the-village.ru/2021/12/17/unmuted-btn.svg) center no-repeat';
+          mutebtn.style.backgroundSize = '100% 100%';
+        } else {
+          audio.muted = true;
+          console.log('Выкл аудио');
+          mutebtn.style.background =
+            'url(https://cdn.the-village.ru/the-village.ru/2021/12/17/sound-btn_0.svg) center no-repeat';
+          mutebtn.style.backgroundSize = '100% 100%';
+        }
+      }
+      function seek(event) {
+        if (seeking) {
+          seekslider.value = event.clientX - seekslider.offsetLeft;
+          seekto = audio.duration * (seekslider.value / 100);
+          audio.currentTime = seekto;
+        }
+      }
+
+      function seektimeupdate() {
+        var nt = audio.currentTime * (100 / audio.duration);
+        seekslider.value = nt;
+      }
+      console.log('audio.duration ' + audio.duration);
+      console.log('audio.duration ' + audio.duration);
+    }
+    window.addEventListener('load', initAudioPlayer);
   }
 
   configOfEventListeners(false, {
